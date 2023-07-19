201c01e0ca5e4505ac2ac50ab44313dbf15198b7
changlog <changlog0510@gmail.com>
2023-07-19 14:09:19 +0900

    깃허브 업로드

---
+++ src/main/java/jenkins/test/controller/JenkinsController.java
@@ -0,0 +1,13 @@
+package jenkins.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JenkinsController {
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+}
